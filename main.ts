25124ead38a66d3c47973b3e7491daf15dc9892e
Isa-teacher <152090968+Isa-teacher@users.noreply.github.com>
2023-11-26 18:46:16 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        # # . # .
+        . # # # .
+        # . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        . # # # #
+        . # . # .
+        . . . . .
+        `)
+})
